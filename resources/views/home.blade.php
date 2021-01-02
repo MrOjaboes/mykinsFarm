2dfd32237e124e6a75a34a1591d2bf1bb1305c7d
Engees <e.ojabo@icredenceng.com>
2021-01-02 03:52:17 +0100

    Added cart UI

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,31 +1,9 @@
-@extends('layouts.auth')
-@section('content')
+<a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+document.getElementById('logout-form').submit();">
+<i class="nav-icon fas fa-sign-out-alt mr-2"></i>
+Logout
+</a>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <span class="float-end"><a href="{{route('cart.list')}}">Cart</a></span>
-            </div>
-        </div>
-        <div class="row">
-            @foreach ($products as $product)
-
-               <div class="col-md-4">
-                <div class="card">
-                    <img src="{{ asset('/storage/products/' . $product->photo) }}" alt="" class="img-thumbnail img-100">
-
-                    <div class="card-body">
-                        <div class="card-title">{{ $product->name }}</div>
-                        <div class="card-text">{{ $product->price }}</div>
-                    </div>
-                    <div class="card-footer"> <a href="{{ route('cart.add', $product->id) }}">Add To cart</a>
-                    </div>
-                </div>
-               </div>
-
-
-
-            @endforeach
-        </div>
-    </div>
-@endsection
+<form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+@csrf
+</form>
