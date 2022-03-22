001a58166f0fe25b502a64166423d36d1a37434a
engees <ojabo.es@gmail.com>
2022-03-22 07:59:15 +0100

    Added pass reset page

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.auth')
 
 @section('content')
-<div class="container">
+<div class="container py-5">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -51,7 +51,7 @@
 
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn" style="background-color:#891e19;color:white">
                                     {{ __('Reset Password') }}
                                 </button>
                             </div>
